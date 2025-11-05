93744cc8b3f82046308cb7e722d7a7657c44b586
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2025-11-06 01:07:19 +0530

    command not found msg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
+        Scanner s = new Scanner(System.in);
+        System.out.print("$ ");
+        String command = s.nextLine();
+        System.out.println(command + ": command not found");
     }
 }
