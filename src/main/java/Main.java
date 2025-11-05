4c63e83210157925887821e9e930e1d2378f36ac
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2025-11-06 01:11:20 +0530

    REPL init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
-        System.out.print("$ ");
-        String command = s.nextLine();
+        while (true){
+            System.out.print("$ ");
+            String command = s.nextLine();
+            evaluate(command);
+        }
+
+    }
+
+    public static void evaluate(String command){
         System.out.println(command + ": command not found");
     }
 }
