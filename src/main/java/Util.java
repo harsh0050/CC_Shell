78ea4b47053ebf481562aef12cc93e890f275395
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2025-11-24 01:15:12 +0530

    escape character support

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -5,10 +5,12 @@ public class Util {
 
     public static String[] getArgv(String command){
         List<String> argv = new LinkedList<>();
+        //todo 'quote>' functionality
         getArgv(command, 0, argv);
         return argv.toArray(new String[0]);
     }
-    private static void getArgv(String command, int start, List<String> ls) {
+    /**@throws IllegalArgumentException if there is syntax error in provided command arguments*/
+    private static void getArgv(String command, int start, List<String> ls) throws IllegalArgumentException{
         if (start == command.length()) return;
         boolean singleQuoteOpen = false;
         boolean doubleQuoteOpen = false;
@@ -29,10 +31,17 @@ public class Util {
                 getArgv(command, i, ls);
                 return;
             }
+            if(currChar == '\\' && !(singleQuoteOpen || doubleQuoteOpen)){
+                if(i >= command.length()) {
+                    throw new IllegalArgumentException("Expected the escaped character at the end");
+                }
+                currChar = command.charAt(i++);
+            }
             sb.append(currChar);
         }
         if(!sb.isEmpty()) ls.add(sb.toString());
     }
+
     public static Result validateArgumentCount(int expected, int found) {
         if(found > expected) return new Failure(Constants.TOO_MANY_ARGUMENTS);
         if(found < expected) return new Failure(Constants.TOO_FEW_ARGUMENTS);
